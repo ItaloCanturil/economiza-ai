527b2577956696dc0e04553ab1af1adb83713649
ItaloCanturil <italo.cantur@gmail.com>
2025-10-09 22:43:28 -0300

    feat(ui): add shadcn/ui components and setup base configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Header from "./components/ui/Header";
 import "./index.css";
 
 function App() {
 	return (
-		<div className="bg-blue-400 w-screen h-screen p-2">
-			<h1>Pratica</h1>
+		<div className="bg-gradient-to-t from-slate-50 to-blue-200 w-screen h-screen p-2">
+			<Header />
 		</div>
 	);
 }
---
+++ src/components/ui/Header.tsx
@@ -0,0 +1,11 @@
+import { Navbar01 } from "../shadcn-io/navbar-01";
+
+const Header = () => {
+	return (
+		<div>
+			<Navbar01 />
+		</div>
+	);
+};
+
+export default Header;
