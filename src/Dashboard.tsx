f083a2b976532eb8e3b848d8ba869ad3757486e0
ItaloCanturil <italo.cantur@gmail.com>
2025-10-11 12:22:34 -0300

    feat(dashboard): add expense analytics chart with category selection

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,32 +1,37 @@
 import { Plus } from "lucide-react";
 import { Button } from "./components/ui/button";
+import { ChartBarDefault } from "./components/ui/shadcn-io/bar-chart-02";
 
 const getGreeting = (): string => {
-  const currentHour = new Date().getHours();
+	const currentHour = new Date().getHours();
 
-  if (currentHour >= 5 && currentHour < 12) {
-    return "Good Morning";
-  } else if (currentHour >= 12 && currentHour < 18) {
-    return "Good Afternoon";
-  } else {
-    return "Good Evening";
-  }
+	if (currentHour >= 5 && currentHour < 12) {
+		return "Good Morning";
+	} else if (currentHour >= 12 && currentHour < 18) {
+		return "Good Afternoon";
+	} else {
+		return "Good Evening";
+	}
 };
 
 const dashboard = () => {
-  const greeting = getGreeting();
+	const greeting = getGreeting();
 
-  return (
-    <div className="flex flex-col sm:flex-row items-center">
-      <div className="text-lg flex justify-between items-center w-full">
-        <h1>{greeting}, Italo!</h1>
-        <Button variant="secondary">
-          <Plus className="mr-2 h-4 w-4" />
-          Add Expense
-        </Button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="sm:flex flex-col items-center justify-center">
+			<div className="text-lg flex justify-between items-center w-full max-w-[82rem]">
+				<h1>{greeting}, User!</h1>
+				<Button variant="secondary">
+					<Plus className="mr-2 h-4 w-4" />
+					Add Expense
+				</Button>
+			</div>
+
+			<section className="mt-4">
+				<ChartBarDefault />
+			</section>
+		</div>
+	);
 };
 
 export default dashboard;
